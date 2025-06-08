25ad20848a6cdb4b96827d1af41aa8cd49eb52b1
Daniel Filipe <danielfsmiguel@gmail.com>
2025-06-08 19:04:01 -0300

    Add port

--- database/db.js
+++ database/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
+  port:  process.env.DB_USER || '',
   password: process.env.DB_PASS || '',
   multipleStatements: true
 });
