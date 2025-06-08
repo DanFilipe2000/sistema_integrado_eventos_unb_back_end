e9039087232b93f554d1e38177698a3cea5e1186
Daniel Filipe <danielfsmiguel@gmail.com>
2025-06-08 18:35:23 -0300

    Fix password

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   port: process.env.DB_PORT || '3306',
-  password: process.env.DB_PASS || 'your_password',
+  password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'eventos_db',
   multipleStatements: true
 });
