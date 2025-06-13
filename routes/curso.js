47177088a44c561b17bd2e53ecdf7e4150404e26
João Pedro Sousa Vasconcelos <joaopedrorbd68@gmail.comm>
2025-06-13 17:55:50 -0300

    Refactor Curso routes to improve organization and readability

--- routes/curso.js
+++ routes/curso.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const CursoController = require('../controllers/CursoController');
 
+router.get('/', CursoController.getAll);
+
+router.get('/:id', CursoController.getById);
+
 router.post('/', CursoController.create);
+
+router.put('/:id', CursoController.update);
+
+router.delete('/:id', CursoController.delete);
 
 module.exports = router;
