b83a9588aded3ab1bf34d089da8079e8739c7a93
Yuri Arruda <114200839+pyurini@users.noreply.github.com>
2025-06-14 19:18:20 -0300

    Adicionado CRUD do Departamento

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const { initDB } = require('./database/db');
 const bodyParser = require('body-parser');
 const participantesRoutes = require('./routes/participantes');
 const cursoRoutes = require('./routes/curso');
+const departamentoRoutes = require('./routes/departamento');
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/participantes', participantesRoutes);
 app.use('/curso', cursoRoutes);
+app.use('/departamento', departamentoRoutes)
 
 app.listen(3000, async () => {
   initDB();
